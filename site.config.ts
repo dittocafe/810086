8ea6acefb0f3cbe55a075199b4f6969c9c226ff6
dittocafe <48623133+dittocafe@users.noreply.github.com>
2023-04-22 11:19:29 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '693e1e28323d40e1995c6dbbdf9278b9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '810086.xyz | 投资学习笔记',
+  domain: 'cn.810086.xyz',
+  author: '810086',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
